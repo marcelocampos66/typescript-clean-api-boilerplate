ccfb5236290ceda2212183287c2bd75eb7f9811e
Marcelo Campos <marcelo.campos.economista@gmail.com>
2022-09-28 09:26:54 -0300

    improove eslint installation

--- src/presentation/controllers/health-check-controller.ts
+++ src/presentation/controllers/health-check-controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Controller, HttpResponse } from '@/src/presentation/protocols';
 import { HttpStatusCode } from '@/src/presentation/helpers/enum-helper';
 import { httpResponse, serverError } from '@/src/presentation/helpers/http-helper';
